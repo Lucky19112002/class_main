b36756e13373effa301377cb6d979b061fb74e47
Lucky Pathan <lckpathan@gmail.com>
2022-07-24 12:39:58 +0530

    bubble_short BestCase Code > tempCodeRunneFile.cpp

---
+++ DAA/temp.cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+#include<chrono>// clock 
+#include<fstream>//for file 
+int sizeofarray1 = 0,sizeofarray2 =0,sizeofarray3 =0,key =0;
+using namespace std;
+using namespace std::chrono;
+void bubbleshort(int a[]) //algotithm function
+{
+  int i,j;
+  for (i = 0; i < sizeofarray1 - 1; i++)
+  {
+    for (j = 0; j < sizeofarray1 - i - 1; j++)
+    {
+      if (a[j] > a[j + 1])
+      {
+        int temp = a[j];
+        a[j] = a[j+1];
+        a[j+1] = temp;
+      }
+    }      
+  }           
+}
+void selectionsort(int arr[])
+{
+  int s2 = sizeofarray2 -1;
+  for(int i =0; i<s2;i++)
+  {
+    int min = i;
+    for (int j = 0; j < sizeofarray2; j++)
+    {
+      if(arr[j]<arr[min])
+      {
+        min =j;
+      }
+    }
+    if (min!=i)
+    {
+      int temp = arr[min];
+      arr[min] = arr[i];
+      arr[i] = temp;
+    }
+  }
+}
+void insertsort(int arr[])
+{
+  int j =0;
+  for (int i = 1; i < sizeofarray3; i++)
+  {
+    key = arr[i];  // picking the element 
+    j = i-1;
+    while (j>=0 && arr[j]>key)
+    {
+      arr[j+1] = arr[j];
+      j = j-1;
+    }
+    arr[j+1] = key;
+  }
+}
+int main()
+{
+  {//bubble short 
+    ofstream bf("/1.bat",ios::app);
+    bf<<"#Bubble sort in milliseconds"<<endl;
+    cout<<endl<<"Working on bubble sort ....";
+    for(sizeofarray1 =10000;sizeofarray1<=50000;sizeofarray1=sizeofarray1+10000)
+    {
+      int bubbleshortarray[sizeofarray1];
+      for (int i = 0; i < sizeofarray1; i++)   // Loop for bubbleshortarray
+      {
+        bubbleshortarray[i] =rand() % 1000;
+      }
+      auto start = high_resolution_clock::now();
+      ios_base::sync_with_stdio(false);
+      bubbleshort(bubbleshortarray);
+      auto stop = high_resolution_clock::now();
+      auto duration = duration_cast<chrono::milliseconds>(stop - start);
+      bf<<sizeofarray1<<" "<<fixed<<duration.count()<<endl;
+      cout<<".";
+    }
+    bf.close();
+    cout<<".... "<<"Done";
+  }
+  {//selection sort 
+    ofstream bf("/2.bat",ios::app);
+    bf<<"#selection sort in milliseconds"<<endl;
+    cout<<endl<<"Working on selection sort ....";
+    int count =0;
+    for(sizeofarray2 =10000;sizeofarray2<=50000;sizeofarray2=sizeofarray2+10000)
+    {
+      int selectshortarray[sizeofarray2];
+      for (int i = 0; i < sizeofarray2; i++,count++)   // Loop for bubbleshortarray
+      {
+        selectshortarray[i] =count;
+      }
+      auto start = high_resolution_clock::now();
+      ios_base::sync_with_stdio(false);
+      bubbleshort(selectshortarray);
+      auto stop = high_resolution_clock::now();
+      auto duration = duration_cast<chrono::milliseconds>(stop - start);
+      bf<<sizeofarray2<<" "<<fixed<<duration.count()<<endl;
+      cout<<".";
+    }
+    bf.close();
+    cout<<".... "<<"Done";
+  }
+  {//insertion sort 
+    ofstream bf("3.bat",ios::app);
+    bf<<"#insertion sort in milliseconds"<<endl;
+    cout<<endl<<"Working on insertion sort ....";
+    for(sizeofarray3 =10000;sizeofarray3<=50000;sizeofarray3=sizeofarray3+10000)
+    {
+      int insertsortarray[sizeofarray3];
+      for (int i = 0; i < sizeofarray3; i++)   // Loop for bubbleshortarray
+      {
+        insertsortarray[i] =rand() % 10;
+      }
+      auto start = high_resolution_clock::now();
+      ios_base::sync_with_stdio(false);
+      insertsort(insertsortarray);
+      auto stop = high_resolution_clock::now();
+      auto duration = duration_cast<chrono::milliseconds>(stop - start);
+      bf<<sizeofarray3<<" "<<fixed<<duration.count()<<endl;
+      cout<<".";
+    }
+    bf.close();
+    cout<<".... "<<"Done";
+  }
+  cout<<endl<<"Ploting the graph...";
+  system("gnuplot -p -e \"set ylabel 'mileseconds' ; set xlabel 'data set' ; p 'C:/Users/karim/Desktop/class/DAA/1.bat' using 1:2 w lp lc rgb 'red' lw 2 pt 7 title 'bubble','C:/Users/karim/Desktop/class/DAA/2.bat' using 1:2 w lp lc rgb 'blue' lw 2 pt 7 title 'selection short','C:/Users/karim/Desktop/class/DAA/3.bat' using 1:2 w lp lc rgb 'green' lw 2 pt 7 title 'insertion short'\"");
+  return 0;
+}
--- DAA/tempCodeRunnerFile.cpp
+++ DAA/tempCodeRunnerFile.cpp
@@ -1,17 +1,75 @@
-for (int i = 0; i < sizeofarray1; i++)   // Loop for bubbleshortarray 1000 to 1
-  // {
-  //   bubbleshortarray1[i] = j;
-  //   j--;
-  // }
-  // auto start = high_resolution_clock::now();
-  // ios_base::sync_with_stdio(false);
-  // bubbleshort(bubbleshortarray1);
-  // auto stop = high_resolution_clock::now();
-  // auto duration = duration_cast<microseconds>(stop - start);
-  
-  // cout<<"Time taken by function 1000 to 0 is : "<<fixed<<duration.count()<< " microseconds" << endl;
-  // for (int i = 0; i < sizeofarray1; i++)   // Loop for bubbleshortarray
-  // {
-  //   bubbleshortarray2[i] = x;
-  //   x++;
-  // }
+#include<iostream>
+#include<chrono>// clock 
+#include<fstream>//for file 
+int sizeofarray1 = 0,sizeofarray1_2 = 0,sizeofarray1_3 =0;
+using namespace std;
+using namespace std::chrono;
+void bubbleshort(int a[],int s) //algotithm function
+{
+  int i,j;
+  for (i = 0; i < s - 1; i++)
+  {
+    for (j = 0; j < s - i - 1; j++)
+    {
+      if (a[j] > a[j + 1])
+      {
+        int temp = a[j];
+        a[j] = a[j+1];
+        a[j+1] = temp;
+      }
+    }      
+  }           
+}
+
+int main()
+{
+  {//bubble short 
+    cout<<"Working on bubble sort avg case ....";
+    for(sizeofarray1 =10000;sizeofarray1<=50000;sizeofarray1=sizeofarray1+10000)
+    {
+      ofstream bf("file_out/bsa.txt",ios::app);
+      int bubbleshortarray[sizeofarray1];
+      for (int i = 0; i < sizeofarray1; i++)   // Loop for bubbleshortarray
+      {
+        bubbleshortarray[i] =rand() % 1000;
+      }
+      auto start = high_resolution_clock::now();
+      ios_base::sync_with_stdio(false);
+      bubbleshort(bubbleshortarray,sizeofarray1);
+      auto stop = high_resolution_clock::now();
+      auto duration = duration_cast<chrono::milliseconds>(stop - start);
+      bf<<sizeofarray1<<" "<<fixed<<duration.count()<<endl;
+      cout<<endl<<sizeofarray1<<" "<<fixed<<duration.count();
+      bf.close();
+    }
+  }
+  //bubble short 
+  {
+    cout<<endl<<"Working on bubble sort best case  ....";
+    int bestCase = 0,sizeofarray1_1 = 0;
+    for(sizeofarray1_1 =10000;sizeofarray1_1<=50000;sizeofarray1_1=sizeofarray1_1+10000)
+    {
+      ofstream bc("file_out/bsb.txt",ios::app);
+      int bubbleshortarray[sizeofarray1_1];
+      for (int i = 0; i < sizeofarray1_1; i++)   // Loop for bubbleshortarray
+      {
+        bubbleshortarray[i] =bestCase;
+        bestCase = bestCase+1;
+      }
+      auto start = high_resolution_clock::now();
+      ios_base::sync_with_stdio(false);
+      bubbleshort(bubbleshortarray,sizeofarray1_1);
+      auto stop = high_resolution_clock::now();
+      auto duration = duration_cast<chrono::milliseconds>(stop - start);
+      bc<<sizeofarray1_1<<" "<<fixed<<duration.count()<<endl;
+      cout<<endl<<sizeofarray1_1<<" "<<fixed<<duration.count();
+      bc.close();    
+    }
+    
+  }
+    cout<<".... "<<"Done";
+
+  cout<<endl<<"Ploting the graph...";
+  system(" gnuplot -p -e \"set ylabel 'mileseconds' ; set xlabel 'data set' ; p 'G:/Sem_5/DAA/file_out/bsb.txt' using 1:2 w lp lc rgb 'blue' lw 2 pt 7 title 'bubble short best case','G:/Sem_5/DAA/file_out/bsa.txt' using 1:2 w lp lc rgb 'red' lw 2 pt 7 title 'bubble short avg case'\"");
+  return 0;
+}
