1533a8951628ed31e76166570d12f35f5cb6a90a
Lucky Pathan <lckpathan@gmail.com>
2022-08-23 17:36:32 +0530

    WP-lab 23-Aug-22

---
+++ WP/JS/semple_lab.js
@@ -0,0 +1,8 @@
+function tagstyl(){
+    var data1 ="String number 1.";
+    var data2 ="String number 2.";
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    document.getElementById("01").innerHTML= "1. Splice of string : "+ fruits.splice(0, 3);
+    document.getElementById("2").innerHTML= "2. Slice of string : "+ fruits.slice(0);
+    document.getElementById("3").innerHTML= "3. Date  : "+ new Date() ;  
+}
